c100718deb4b3a741c2d54fffe58b40d461f7deb
Elmer <elmerptuazon@yahoo.com>
2019-03-19 17:23:13 +0800

    img frm db

--- src/component/ShopContent.js
+++ src/component/ShopContent.js
@@ -30,7 +30,8 @@ class ShopContent extends Component {
       shop_title: "",
       shop_price: "",
       shop_file: null,
-      shop_completed: false
+      shop_completed: false,
+      shop_getData: []
     };
 
     this.onChangeShopTitle = this.onChangeShopTitle.bind(this);
@@ -64,18 +65,14 @@ class ShopContent extends Component {
     this.setState({
       shop_completed: true
     });
-
-    console.log("Title: " + this.state.shop_title);
-    console.log("Price: " + this.state.shop_price);
-    console.log("Completed: " + this.state.shop_completed);
   }
 
   onSubmit(e) {
     e.preventDefault();
-
-    console.log("Title: " + this.state.shop_title);
-    console.log("Price: " + this.state.shop_price);
-    console.log("Completed: " + this.state.shop_completed);
+    // console.log("Title: " + this.state.shop_file);
+    // console.log("Title: " + this.state.shop_title);
+    // console.log("Price: " + this.state.shop_price);
+    // console.log("Completed: " + this.state.shop_completed);
 
     const newShop = {
       shop_file: this.state.shop_file,
@@ -96,6 +93,33 @@ class ShopContent extends Component {
     });
   }
 
+  onShowShopList() {
+    return this.state.shop_getData.map(function(val, i) {
+      var strSlice = val.shop_file.slice(5);
+      console.log(strSlice);
+      return (
+        <tr className="border border-primary">
+          <img src={`data:image/jpeg;base64,${strSlice}`} alt="Sample" />
+          <div>{val.shop_price}</div>
+          <div>{val.shop_title}</div>
+        </tr>
+      );
+    });
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/shop")
+      .then(res => {
+        this.setState({
+          shop_getData: res.data
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
   componentWillUnmount() {
     this.setState({
       shop_title: "",
@@ -108,53 +132,65 @@ class ShopContent extends Component {
   render() {
     return (
       <div style={{ marginTop: 20 }}>
-        <h3>Add Item</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <div className="btn btn-primary">
-              <input type="file" onChange={this.onChangeShopImage} />
+        {this.state.shop_getData == "" ? (
+          <div>No Contents Available</div>
+        ) : (
+          <div>
+            <h3>Add Item</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <div className="btn btn-primary">
+                  <input type="file" onChange={this.onChangeShopImage} />
+                </div>
+              </div>
+              <div className="form-group">
+                <label>Product Title: </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={this.state.shop_title}
+                  onChange={this.onChangeShopTitle}
+                />
+              </div>
+              <div className="form-group">
+                <label>Product Price: </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={this.state.shop_price}
+                  onChange={this.onChangeShopPrice}
+                />
+              </div>
+              <div className="form-group">
+                <button
+                  onClick={this.onHandlePreview}
+                  className="btn btn-primary"
+                >
+                  Preview
+                </button>
+              </div>
+              <div className="form-group">
+                <input
+                  type="submit"
+                  className="btn btn-success"
+                  value="Submit Item"
+                />
+              </div>
+            </form>
+            <div className="row">
+              {this.state.shop_completed && (
+                <ImagePreview
+                  shop_file={this.state.shop_file}
+                  shop_title={this.state.shop_title}
+                  shop_price={this.state.shop_price}
+                />
+              )}
+              <table style={{ marginTop: 20 }}>
+                <tbody>{this.onShowShopList()}</tbody>
+              </table>
             </div>
           </div>
-          <div className="form-group">
-            <label>Product Title: </label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.shop_title}
-              onChange={this.onChangeShopTitle}
-            />
-          </div>
-          <div className="form-group">
-            <label>Product Price: </label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.shop_price}
-              onChange={this.onChangeShopPrice}
-            />
-          </div>
-          <div className="form-group">
-            <button onClick={this.onHandlePreview} className="btn btn-primary">
-              Preview
-            </button>
-          </div>
-          <div className="form-group">
-            <input
-              type="submit"
-              className="btn btn-success"
-              value="Submit Item"
-            />
-          </div>
-        </form>
-        <div className="row">
-          {this.state.shop_completed && (
-            <ImagePreview
-              shop_file={this.state.shop_file}
-              shop_title={this.state.shop_title}
-              shop_price={this.state.shop_price}
-            />
-          )}
-        </div>
+        )}
       </div>
     );
   }
